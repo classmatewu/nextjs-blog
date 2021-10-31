3c6ae8116184bf5a0c3ed011a9910621612b7a7f
zelinwu <zelinwu@tencent.com>
2021-10-31 11:39:32 +0800

    feat: 添加css module样式文件

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,19 +1,16 @@
 import Head from 'next/Head'
 import Link from 'next/link'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
   return (
-    <>
-      <Head>
-        <title>First Post</title>
-      </Head>
-
+    <Layout>
       <h1>First Post</h1>
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   )
 }
